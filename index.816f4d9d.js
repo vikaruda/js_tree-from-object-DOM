d2b9c649440ec7f8a238b5c3f2ac9e6ae89e146e
Vika <rudaviktoria0@gmail.com>
2024-09-16 17:26:38 +0300

    make build

---
+++ index.816f4d9d.js
@@ -0,0 +1,2 @@
+document.body.id="tree",function(e,n){e.append(function e(n){var t=document.createElement("ul");for(var r in n){var a=document.createElement("li");a.textContent=r,t.append(a),Object.keys(n[r]).length>0&&a.append(e(n[r]))}return t}(n))}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.816f4d9d.js.map
